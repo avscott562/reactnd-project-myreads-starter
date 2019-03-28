bf39e7d6b3d7ee74d31cc4f293338463c7456d4c
aliciascott <avscott@pacbell.net>
2019-03-28 09:18:26 -0700

    moved shelf and book info to respective components

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -3,7 +3,22 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import escapeRegExp from 'escape-string-regexp'
 import sortBy from 'sort-by'
+import Book from './Book'
 
-class Shelf extends Component {}
+class Shelf extends Component {
+  render() {
+    return(
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">Currently Reading</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <Book/>
+          </ol>
+        </div>
+      </div>
+
+    )
+  }
+}
 
 export default Shelf
